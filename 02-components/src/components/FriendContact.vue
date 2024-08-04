424e32b633a2d34999dad015fe3e7e7e00795bad
engulfedinflames <recona97@gmail.com>
2024-08-04 22:31:32 +0900

    Learn vue components - 1

---
+++ 02-components/src/components/FriendContact.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue";
+
+const detailsAreVisible = ref(false);
+const friend = {
+    id: "manuel",
+    name: "Manuel Lorenz",
+    phone: "01234 5678 991",
+    email: "manuel@example.com",
+};
+
+function toggleDetails() {
+    detailsAreVisible.value = !detailsAreVisible.value;
+}
+</script>
+
+<template>
+    <li>
+        <h2>
+            {{ friend.name }}
+        </h2>
+        <button @click="toggleDetails">{{
+            detailsAreVisible ? "Hide" : "Show"
+        }}</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
